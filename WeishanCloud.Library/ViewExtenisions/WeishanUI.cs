10b98a36637c29d49a00885bfc395ee0544b8a5f
Xiangrui2019 <xiangrui@aiursoft.com>
2019-12-11 20:20:38 +0800

    split core and dashboard for fast

--- WeishanCloud.Library/ViewExtenisions/WeishanUI.cs
+++ WeishanCloud.Library/ViewExtenisions/WeishanUI.cs
@@ -7,22 +7,32 @@ namespace WeishanCloud.Library.ViewExtenisions
 {
     public static class WeishanUI
     {
-        public static IHtmlContent UseDashboardStyleSheet(this RazorPage page, bool includeCore = true)
+        public static IHtmlContent UseCoreStyleSheet(this RazorPage page)
         {
             var serviceLocation = page.Context.RequestServices.GetService<ServiceLocation>();
             var builder = new HtmlContentBuilder()
                 .AppendHtml($"<meta name=\"theme-color\" content=\"#0082c9\">");
-            if (includeCore)
-            {
-                return builder
-                    .StyleSheet($"{serviceLocation.UI}/dist/Core.min.css")
+            
+            return builder
+                .StyleSheet($"{serviceLocation.UI}/dist/Core.min.css");
+        }
+
+        public static IHtmlContent UseCoreJavaScript(this RazorPage page)
+        {
+            var serviceLocation = page.Context.RequestServices.GetService<ServiceLocation>();
+            
+            return new HtmlContentBuilder()
+                .JavaScript($"{serviceLocation.UI}/dist/Core.min.js");
+        }
+        
+        public static IHtmlContent UseDashboardStyleSheet(this RazorPage page)
+        {
+            var serviceLocation = page.Context.RequestServices.GetService<ServiceLocation>();
+            var builder = new HtmlContentBuilder()
+                .AppendHtml($"<meta name=\"theme-color\" content=\"#0082c9\">");
+            
+            return builder
                     .StyleSheet($"{serviceLocation.UI}/dist/Dashboard.min.css");
-            }
-            else
-            {
-                return builder
-                    .StyleSheet($"{serviceLocation.UI}/dist/Dashboard.min.css");
-            }
         }
 
         public static IHtmlContent UseDashboardJavaScript(this RazorPage page, bool includeCore = true)
